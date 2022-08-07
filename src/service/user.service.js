6fa2b384a4d7030e78813965ce8b76de808e8708
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-07 17:12:00 +0530

    Implement createUser

---
+++ src/service/user.service.js
@@ -0,0 +1,15 @@
+import User from "../model/user.js";
+import bcrypt from "bcrypt";
+
+export const createUser = async (username, password) => {
+	const saltRounds = 10
+	const passwordHash = await bcrypt.hash(password, saltRounds)
+
+	const user = new User({
+		username,
+		passwordHash,
+		isAdmin: false
+	})
+
+	return user.save()
+}
