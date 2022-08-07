016f3009f5247048934fa1e26e790fe16f83e457
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-07 17:12:09 +0530

    Implement user schema

---
+++ src/model/user.js
@@ -0,0 +1,32 @@
+import uniqueValidator from 'mongoose-unique-validator'
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		minlength: 3,
+		required: true
+	},
+	passwordHash: {
+		type: String, required: true
+	},
+	isAdmin: {
+		type: Boolean,
+		required: true,
+	}
+})
+
+userSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
